3a8c1a9ddb445af839df422415ee4b356c4aceaf
Jadson Alex <jadsonalex1312@gmail.com>
2025-09-08 13:34:45 +0000

    config

---
+++ config.py
@@ -0,0 +1,8 @@
+LINK_WMS = 'http://200.143.168.151:8880/siltwms/'
+
+GOOGLE_DRIVE_PATH = r"G:\Meu Drive"
+
+LOGINS_WMS = ['user_1', 'user_2']
+SENHAS_WMS = ['passw_1', 'passw_2']
+ID_TOKEN_WMS = ['id_1', 'id_2']
+TOKENS_SENHAS = ['token_1', 'token_2']
